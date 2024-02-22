77d05b0f3c84dfb22892cc394c8124e5340b657f
Aleksey Nikotin <exilonone@gmail.com>
2024-02-22 20:20:44 +0400

    first commit

---
+++ src/cyclic_complexity/ExampleOne.java
@@ -0,0 +1,86 @@
+package cyclic_complexity;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class ExampleOne {
+
+  public static final String ANSI_RESET = "\u001B[0m";
+  public static final String ANSI_RED = "\u001B[31m";
+
+  private final Map<String, Integer> indexMap = new HashMap<>();
+
+  public boolean hasNecessaryColumn(Sheet sheet, String fileName, String columnName) {
+    Row headerRow = sheet.getRow(0);
+    if (Objects.nonNull(headerRow)) {
+      for (Cell cell : headerRow) {
+        if (CellType.STRING == cell.getCellType()) {
+          String cellName = cell.getStringCellValue().trim().toLowerCase();
+          if (columnName.equals(cellName) && indexMap.containsKey(cellName)) {
+            System.out.printf(
+                ANSI_RED + "Колонка '%s' дублируется в файле '%s'. Конфликт данных.%n" + ANSI_RESET,
+                columnName, fileName);
+            return false;
+          }
+
+          if (columnName.equals(cellName)) {
+            indexMap.put(cellName, cell.getColumnIndex());
+          }
+        }
+      }
+    }
+
+    if (indexMap.containsKey(columnName)) {
+      System.out.printf("Колонка '%s' найдена в файле '%s'.%n", columnName, fileName);
+      return true;
+    } else {
+      System.out.printf(ANSI_RED + "Колонка '%s' не найдена в файле '%s'.%n" + ANSI_RESET,
+          columnName,
+          fileName);
+      return false;
+    }
+  }
+
+  // Исправленный метод
+  public boolean hasNecessaryColumnAfterRefactoring(Sheet sheet, String fileName,
+      String columnName) {
+    Row headerRow = sheet.getRow(0);
+    if (Objects.nonNull(headerRow)) {
+      return false;
+    }
+
+    for (Cell cell : headerRow) {
+      if (CellType.STRING != cell.getCellType()) {
+        continue;
+      }
+
+      String cellName = cell.getStringCellValue().trim().toLowerCase();
+      if (checkCellExistsAndColumnNameMatch(cellName, columnName)) {
+        System.out.printf(
+            ANSI_RED + "Колонка '%s' дублируется в файле '%s'. Конфликт данных.%n" + ANSI_RESET,
+            columnName, fileName);
+        return false;
+      }
+
+      if (columnName.equals(cellName)) {
+        indexMap.put(cellName, cell.getColumnIndex());
+      }
+    }
+
+    if (indexMap.containsKey(columnName)) {
+      System.out.printf("Колонка '%s' найдена в файле '%s'.%n", columnName, fileName);
+      return true;
+    }
+
+    System.out.printf(ANSI_RED + "Колонка '%s' не найдена в файле '%s'.%n" + ANSI_RESET, columnName,
+        fileName);
+    return false;
+  }
+
+  private boolean checkCellExistsAndColumnNameMatch(String cellName, String columnName) {
+    return columnName.equals(cellName) && indexMap.containsKey(cellName);
+  }
+
+}
+
